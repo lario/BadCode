3f5c638400ecb6203759c9b6d988134ca0e52115
Andrei Larionescu <andrei@larionescu.ro>
2014-10-28 19:40:10 +0200

    first examples

---
+++ BadCode/BadCode/CommonMistakes/CM01_q.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCode.CommonMistakes
+{
+    class CM01_q
+    {
+        #region Bad
+
+        public void Bad1()
+        {
+            string input = Console.ReadLine();
+
+            var result = int.Parse(input);
+        } 
+
+        #endregion
+
+        #region Bad
+
+        public void Bad2()
+        {
+            string input = Console.ReadLine();
+
+            int result;
+            try
+            {
+                result = int.Parse(input);
+            }
+            catch (Exception)
+            {
+                result = 0;
+            }            
+        } 
+
+        #endregion
+
+        #region Good
+
+        public void Good()
+        {
+            string input = Console.ReadLine();
+
+            int result;
+            if (Int32.TryParse(input, out result))
+            {
+                // use the value
+            }
+        } 
+
+        #endregion
+    }
+}
---
+++ BadCode/BadCode/CommonMistakes/CM02.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCode.CommonMistakes
+{
+    class CM02
+    {
+        #region Concaternation
+
+        void Bad1()
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                result += i;
+            }
+        }
+
+        void Good()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                result.Append(i.ToString());
+            }
+        }
+
+        #endregion
+
+        #region Format
+
+        void Bad()
+        {
+            var elementType = "Employer";
+            var id = 1;
+
+            var result = "Are you sure you want to remove " + elementType + " with id: " + id + "?";
+        }
+
+        void Good()
+        {
+            var elementType = "Employer";
+            var id = 1;
+
+            var result = String.Format("Are you sure you want to remove {0} with id: {1}?", elementType, id);
+        }
+
+        #endregion
+
+        #region ML String
+
+        void Bad()
+        {
+            var result = "SELECT column_name, aggregate_function(column_name) " +
+                            "FROM table_name " +
+                            "WHERE column_name operator value " +
+                            "GROUP BY column_name;";
+        }
+
+        void Good()
+        {
+            var result = @"SELECT column_name, aggregate_function(column_name)
+                            FROM table_name
+                            WHERE column_name operator value
+                            GROUP BY column_name;";
+        }
+
+        #endregion
+
+        #region Concat list
+
+        void Bad()
+        {
+            var values = new string[]{"value1", "value2", "value3"};
+
+            var result = String.Empty;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result += values[i];
+
+                if (i < values.Length - 1)
+                    result += ",";
+            }
+        }
+
+        void Bad()
+        {
+            var values = new string[] { "value1", "value2", "value3" };
+
+            var result = String.Empty;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result += values[i] + ",";
+            }
+
+            result.TrimEnd(',');
+        }
+
+        void Good()
+        {
+            var values = new string[] { "value1", "value2", "value3" };
+
+            var result = String.Join(",", values);
+        }
+
+        #endregion
+
+        #region Comparison
+
+        //show in turkey
+
+        #endregion
+    }
+}
